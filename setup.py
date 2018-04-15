4b65e130ae62d8efb080c886285d2fdfa2e3e103
Thomas Liske <thomas@fiasko-nw.net>
2018-04-15 20:58:31 +0200

    Prepare PyPi upload.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'apds9960',
+    packages = ['apds9960'],
+    version = '0.1',
+    description = 'Python APDS-9960 Library',
+    author = 'Thomas Liske',
+    author_email = 'thomas@fiasko-nw.net',
+    url = 'https://github.com/liske/python-apds9960/', # use the URL to the github repo
+    download_url = 'https://github.com/liske/python-apds9960/archive/0.1.tar.gz', # I'll explain this in a second
+    keywords = ['apds9960'], # arbitrary keywords
+    classifiers = [],
+)
